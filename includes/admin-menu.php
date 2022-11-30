89e6a404a7e8b36a5bbebdd3d0e950aa653e95d4
Ryoma0305 <ryomaaa0602@gmail.com>
2022-11-30 13:17:28 +0900

    first commit

---
+++ includes/admin-menu.php
@@ -0,0 +1,133 @@
+<?php
+// ---------------------------------------
+// 設定画面
+// ---------------------------------------
+// function msk_show_config()
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+wp_enqueue_media();
+?>
+
+<div class="wrap">
+    <div id="icon-options-general" class="icon32"><br /></div><h2>OPG自動生成 設定</h2>
+        <form action="" method="post">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row"><label for="inputtext">使用するフォント<br>（CSS3 Web Font推奨）</label></th>
+                    <td>
+                        <input type="button" name="ogp_font_url_slect" value="選択" /><br>
+                        <input name="ogp_font_url" type="text" value="" style="width:60%" readonly="readonly"/>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="inputtext">テンプレート画像</label></th>
+                    <td>
+                        <input type="button" name="ogp_image_url_slect" value="選択" /><br>
+                        <input name="ogp_image_ids" type="hidden" value="" readonly="readonly"/>
+                        <div id="ogp_image_url_thumbnail" class="uploded-thumbnail">
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <p class="submit"><input type="submit" name="Submit" class="button-primary" value="変更を保存" /></p>
+        </form>
+    </div>
+</div>
+<!-- <script type="text/javascript">
+(function ($) {
+    var image_uploader;
+    var font_uploader;
+    $("input:button[name=ogp_image_url_slect]").click(function(e) {
+        e.preventDefault();
+        if (image_uploader) {
+            image_uploader.open();
+            return;
+        }
+        image_uploader = wp.media({
+            title: "画像を選択してください",
+            /* ライブラリの一覧は画像のみにする */
+            library: {
+                type: "image",
+                author: userSettings.uid
+            },
+            button: {
+                text: "画像の選択"
+            },
+            multiple: true
+        });
+        image_uploader.on("select", function() {
+            /* テキストフォームと表示されたサムネイル画像があればクリア */
+            $("input:hidden[name=ogp_image_ids]").val("");
+            $("#ogp_image_url_thumbnail").empty();
+
+            /* file の中に選択された画像の各種情報が入っている */
+            var images = image_uploader.state().get("selection");
+            images.each(function(file){
+                //console.log(file.attributes);
+                var id = file.attributes.id;
+                var title = file.attributes.title;
+                var url = file.attributes.sizes.thumbnail.url;
+
+                /* テキストフォームに画像の URL を表示 */
+                var idTmp = $("input:hidden[name=ogp_image_ids]").val();
+                if( idTmp != "" ) idTmp += ',';
+                $("input:hidden[name=ogp_image_ids]").val(idTmp+id);
+                /* プレビュー用に選択されたサムネイル画像を表示 */
+                $("#ogp_image_url_thumbnail").append('<div class="box"><img src="'+url+'" alt="'+title+'"style="height:128px;"/><p>'+title+'</p><div>');
+            });
+        });
+        image_uploader.open();
+    });
+    $("input:button[name=ogp_font_url_slect]").click(function(e) {
+        e.preventDefault();
+        if (font_uploader) {
+            font_uploader.open();
+            return;
+        }
+        font_uploader = wp.media({
+            title: "フォントを選択してください",
+            /* ライブラリの一覧はフォントのみにする */
+            library: {
+                search: "otf ttf",
+                author: userSettings.uid
+            },
+            button: {
+                text: "フォントの選択"
+            },
+            /* 選択できる画像は 1 つだけにする */
+            multiple: false
+        });
+        font_uploader.on("select", function() {
+            $("input:text[name=ogp_font_url]").val("");
+
+            var fonts = font_uploader.state().get("selection");
+            /* file の中に選択された画像の各種情報が入っている */
+            fonts.each(function(file){
+                var path = file.attributes.url;
+                $("input:text[name=ogp_font_url]").val(path);
+            });
+        });
+        font_uploader.open();
+    });
+})(jQuery);
+</script> -->
+<style type="text/css">
+    .box {
+        position: relative;
+        display: inline-block;
+        _display: inline;
+    }
+    .box p {
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 10px;
+        background-color: #000;
+        color: #fff;
+    }
+</style>
+
+<?php
+return;
