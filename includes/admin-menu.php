92b7f47d12529d40a210f01adbbae66f8a7db1d0
Ryoma0305 <ryomaaa0602@gmail.com>
2022-12-25 16:04:25 +0900

    レビュー修正:Variables and options must be escaped when echo'd、Calling file locations poorly

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -20,14 +20,13 @@ $preview_sample_text = filter_input( INPUT_POST, 'preview_sample_text', FILTER_S
 
 if( $original_image_id && $font_url && $font_size && $ogp_font_color && $ogp_new_line_char_length ){
     check_admin_referer('ogp_config');
-    $update_preview_image_url = WP_PLUGIN_URL . '/ogp-image-generator/img/preview/ogp-example.jpg' . '?' . date('YmdHis');
-
+    $preview_image_url = plugins_url('/ogp-image-generator') . '/img/preview/ogp-example.jpg?' . date('YmdHis');
     update_option(self::PLUGIN_ORIGINAL_IMAGE_ID, $original_image_id);
     update_option(self::PLUGIN_FONT_URL, $font_url);
     update_option(self::PLUGIN_FONT_SIZE, $font_size);
     update_option(self::PLUGIN_FONT_COLOR, $ogp_font_color);
     update_option(self::PLUGIN_NEWLINE_CHAR_LENGTH, $ogp_new_line_char_length);
-    update_option(self::PLUGIN_PREVIEW_IMAGE_URL, $update_preview_image_url);
+    update_option(self::PLUGIN_PREVIEW_IMAGE_URL, $preview_image_url);
     if($preview_sample_text){
         update_option(self::PLUGIN_PREVIEW_SAMPLE_TEXT, $preview_sample_text);
     }
@@ -61,10 +60,6 @@ foreach($ids as $id){
 
 $ogp_preview_image_url = get_option(self::PLUGIN_PREVIEW_IMAGE_URL, null);
 $ogp_preview_sample_text = get_option(self::PLUGIN_PREVIEW_SAMPLE_TEXT, null);
-
-$file = __FILE__;
-$file_path = strstr(__FILE__, 'includes', true);
-$file_path_to_img = $file_path . 'img/';
 ?>
             <div class="form-table-wrapper">
                 <table class="form-table">
@@ -72,7 +67,7 @@ $file_path_to_img = $file_path . 'img/';
                         <th scope="row"><label for="inputtext">Font</label></th>
                         <td>
                             <input type="button" name="ogp_font_url_slect" value="Upload" /><br>
-                            <input name="ogp_font_url" type="text" value="<?php echo esc_url($ogp_font_url) ?>" style="width:60%; margin-top:4px;" readonly="readonly"/>
+                            <input name="ogp_font_url" type="text" value="<?php echo esc_attr($ogp_font_url) ?>" style="width:60%; margin-top:4px;" readonly="readonly"/>
                             <p style="margin-top:10px;">Uploadable File Formats : ttf</p>
                             <?php if (empty($ogp_font_url)) : ?>
                             <p class="error-txt">Required Field</p>
@@ -82,7 +77,7 @@ $file_path_to_img = $file_path . 'img/';
                     <tr>
                         <th scope="row"><label for="inputtext">Font Size</label></th>
                         <td>
-                            <input name="ogp_font_size" type="number" id="ogp_font_size" value="<?php echo esc_html($ogp_font_size) ?>" class="regular-text"/>
+                            <input name="ogp_font_size" type="number" id="ogp_font_size" value="<?php echo esc_attr($ogp_font_size) ?>" class="regular-text"/>
                             <?php if (empty($ogp_font_size)) : ?>
                             <p class="error-txt">Required Field</p>
                             <?php endif; ?>
@@ -92,7 +87,7 @@ $file_path_to_img = $file_path . 'img/';
                     <tr>
                         <th scope="row"><label for="inputtext">Font Color</label></th>
                         <td>
-                            <input class="input_ogp_font_color" name="ogp_font_color" type="color" id="ogp_font_color" value="<?php  echo esc_html($ogp_font_color) ?>" class="regular-text"/>
+                            <input class="input_ogp_font_color" name="ogp_font_color" type="color" id="ogp_font_color" value="<?php  echo esc_attr($ogp_font_color) ?>" class="regular-text"/>
                             <?php if (empty($ogp_font_color)) : ?>
                             <p class="error-txt">Required Field</p>
                             <?php endif; ?>
@@ -101,7 +96,7 @@ $file_path_to_img = $file_path . 'img/';
                     <tr>
                         <th scope="row"><label for="inputtext">Number Of Characters To Be New-lined</label></th>
                         <td>
-                            <input name="ogp_new_line_char_length" type="number" id="ogp_new_line_char_length" value="<?php  echo esc_html($ogp_new_line_char_length) ?>" class="regular-text"/>
+                            <input name="ogp_new_line_char_length" type="number" id="ogp_new_line_char_length" value="<?php  echo esc_attr($ogp_new_line_char_length) ?>" class="regular-text"/>
                             <?php if (empty($ogp_new_line_char_length)) : ?>
                             <p class="error-txt">Required Field</p>
                             <?php endif; ?>
@@ -111,10 +106,10 @@ $file_path_to_img = $file_path . 'img/';
                         <th scope="row"><label for="inputtext">Background Image</label></th>
                         <td>
                             <input type="button" name="ogp_image_url_slect" value="Upload" /><br>
-                            <input name="original_image" type="hidden" value="<?php echo esc_html($original_image) ?>" readonly="readonly"/>
+                            <input name="original_image" type="hidden" value="<?php echo esc_attr($original_image) ?>" readonly="readonly"/>
                             <div id="ogp_image_url_thumbnail" class="uploded-thumbnail">
                             <?php foreach ($ogp_image_urls as $key => $url): ?>
-                                <input name="original_image_id" type="text" value="<?php echo esc_url($url) ?>" style="width:60%; margin-top:4px;" readonly="readonly"/>
+                                <input name="original_image_id" type="text" value="<?php echo esc_attr($url) ?>" style="width:60%; margin-top:4px;" readonly="readonly"/>
                             <?php endforeach ?>
                             </div>
                             <?php if (empty($original_image)) : ?>
@@ -125,14 +120,14 @@ $file_path_to_img = $file_path . 'img/';
                     <tr>
                         <th scope="row"><label for="inputtext">Sample Text For Preview</label></th>
                         <td>
-                            <input name="preview_sample_text" type="text" id="preview_sample_text" value="<?php echo esc_html($ogp_preview_sample_text) ?>" class="regular-text" style="display:block; margin-bottom:8px; width: 530px;"/>
+                            <input name="preview_sample_text" type="text" id="preview_sample_text" value="<?php echo esc_attr($ogp_preview_sample_text) ?>" class="regular-text" style="display:block; margin-bottom:8px; width: 530px;"/>
                         </td>
                     </tr>
                     <?php if (!empty($ogp_preview_image_url)) : ?>
                     <tr>
                         <th scope="row"><label for="inputtext">Preview Image</label></th>
                         <td>
-                            <div class="box"><img src="<?php echo $ogp_preview_image_url; ?>" alt="" style="height:280px;"/></div>
+                            <div class="box"><img src="<?php echo esc_url($ogp_preview_image_url); ?>" alt="" style="height:280px;"/></div>
                         </td>
                     </tr>
                     <?php endif; ?>
